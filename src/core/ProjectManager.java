c80344bb76dff872eac9327fe5db0108a4eed3da
Christian Anyanwu <c.anyanwu@wustl.edu>
2018-11-01 17:10:26 -0500

    Created all of the main classes needed for Core

--- src/core/ProjectManager.java
+++ src/core/ProjectManager.java
@@ -1,10 +1,24 @@
 package core;
 
+import output.OutputManager;
+import input.InputManager;
+
 public class ProjectManager {
 
 	public static void main(String[] args) {
-		System.out.println("First edit");
+		ProjectManager pm  = new ProjectManager();
+		pm.run();
 
 	}
 
+	InputManager im;
+	OutputManager om;
+	
+	Base base;
+	
+	private void run() {
+		
+	}
+	
+	
 }
