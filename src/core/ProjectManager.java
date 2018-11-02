c04404257b19bc1af9961b7ccf6296984afc7ea4
Christian Anyanwu <c.anyanwu@wustl.edu>
2018-11-01 21:30:57 -0500

    started Input manager

--- src/core/ProjectManager.java
+++ src/core/ProjectManager.java
@@ -1,24 +1,34 @@
 package core;
 
 import output.OutputManager;
+
+import java.util.ArrayList;
+
+import base.Base;
+import command.CommandManager;
 import input.InputManager;
 
 public class ProjectManager {
 
 	public static void main(String[] args) {
-		ProjectManager pm  = new ProjectManager();
+		ProjectManager pm = new ProjectManager();
 		pm.run();
 
 	}
 
 	InputManager im;
 	OutputManager om;
-	
+	CommandManager cm;
+
 	Base base;
-	
+
 	private void run() {
-		
+		//main loop just no while for now
+		ArrayList<String> cmds = im.getCommands();
+	
+		if (cmds.size() > 0) {
+
+		}
 	}
-	
-	
+
 }
--- src/input/InputManager.java
+++ src/input/InputManager.java
@@ -1,5 +1,12 @@
 package input;
 
+import java.util.ArrayList;
+
 public class InputManager {
+
+	public ArrayList<String> getCommands() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
